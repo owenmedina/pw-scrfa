2a9e37c12c22399b1924fce6c842d7437236fea8
Owen Medina <owensmedi@gmail.com>
2021-05-12 17:16:55 -0700

    Add component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Header from "./components/Header";
+import MainHeading from "./components/MainHeading";
+import Pages from "./components/Pages";
+import Info from "./components/Info";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
+      <MainHeading />
+      <Pages />
+      <Info />
+      <Footer />
     </div>
   );
 }
