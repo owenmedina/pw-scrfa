029e76598db54e68b74f7b5daac4dec3e3a67af6
Owen Medina <owensmedi@gmail.com>
2021-05-13 14:30:09 -0700

    Add search button

---
+++ src/components/Buttons/SearchButton.tsx
@@ -0,0 +1,11 @@
+import {ReactComponent as SearchSVG} from './assets/svg/search.svg';
+import styles from './SearchButton.module.scss';
+
+const SearchButton = () => {
+    return <button className={styles['button-search']}>
+        <span className={`${styles['button-search__text']} ${styles['mr-sm']}`}>Search</span>
+        <SearchSVG className={styles['button-search__svg']}/>
+        </button>;
+}
+
+export default SearchButton;
