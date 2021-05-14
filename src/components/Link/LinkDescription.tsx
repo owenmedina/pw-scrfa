4596a27e30ae70ccca10864126c70718d3870537
Owen Medina <owensmedi@gmail.com>
2021-05-14 14:41:17 -0700

    Style date

--- src/components/Link/LinkDescription.tsx
+++ src/components/Link/LinkDescription.tsx
@@ -7,11 +7,21 @@ interface Props {
 }
 
 const LinkDescription: FC<Props> = ({desc, date}) => {
+    const year = 'numeric';
+    const month = 'short';
+    const day = 'numeric';
+    const locale = 'en-US'
+    const formattedYear = date.toLocaleDateString(locale, {year});
+    const formattedMonth = date.toLocaleDateString(locale, {month});
+    const formattedDay = date.toLocaleDateString(locale, {day});
     return <div>
         {desc && <div className={styles['description__text']}>
             {desc}
             </div>}
-        {date.toDateString()}
+        <div>
+            <span className={[styles['date__label'], styles.date].join(' ')}>Published: </span>
+            <span className={[styles['date__date'], styles.date].join(' ')}>{`${formattedMonth} ${formattedDay}, ${formattedYear}`}</span>
+        </div>
     </div>;
 }
 
