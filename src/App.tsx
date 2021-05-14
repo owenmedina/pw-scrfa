159eb1a7cd417fe73a34628a9b1a96c4fa73db96
Owen Medina <owensmedi@gmail.com>
2021-05-13 20:08:53 -0700

    Add sections data

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import styles from './App.module.scss';
 import Header from "./components/Header/Header";
 import MainHeading from "./components/Main Heading/MainHeading";
-import Pages from "./components/Sections/Sections";
+import Sections from "./components/Sections/Sections";
 import Info from "./components/Info";
 import Footer from "./components/Footer";
+import Section from './types/Section';
+
+import {ReactComponent as DataSVG} from './assets/svg/data.svg';
+import {ReactComponent as GeographySVG} from './assets/svg/geography.svg';
+import {ReactComponent as ProgramsSVG} from './assets/svg/programs.svg';
+import {ReactComponent as FiscalSVG} from './assets/svg/fiscal.svg';
 
 function App() {
+  const sections = [new Section(<DataSVG />, 'Data & Research'), new Section(<GeographySVG />, 'Geography & Mapping'), new Section(<ProgramsSVG />, 'Programs & Services'), new Section(<FiscalSVG />, 'Legislative Fiscal Impacts')];
   return (
     <div className={styles.App}>
       <Header />
       <MainHeading />
-      <Pages />
+      <Sections sections={sections}/>
       <Info />
       <Footer />
     </div>
