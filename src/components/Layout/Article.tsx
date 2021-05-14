32db6b94480b36c65e3b8a48053a0002a74af35a
Owen Medina <owensmedi@gmail.com>
2021-05-14 12:36:22 -0700

    Add Spacer and ColumnarContent

--- src/components/Layout/Article.tsx
+++ src/components/Layout/Article.tsx
@@ -1,14 +1,21 @@
-import { FC } from "react";
+import { FC, ReactElement } from "react";
 import Heading from '../Typography/Heading';
+import ColumnarContent from './ColumnarContent';
+import Spacer from "../Utilities/Spacer";
 
 interface Props {
     title: string;
     titleStyle: number;
+    content: ReactElement;
 }
 
-const Article: FC<Props> = ({title, titleStyle}) => {
+const Article: FC<Props> = ({title, titleStyle, content}) => {
     return <div>
         <Heading style={titleStyle} text={title} />
+        <Spacer size={'md'} />
+        <ColumnarContent columns={2}>
+            {content}
+        </ColumnarContent>
     </div>;
 }
 
