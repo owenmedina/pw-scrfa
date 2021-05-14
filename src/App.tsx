5ae93a54c5618f39e731e4dc8a41475b691c61e4
Owen Medina <owensmedi@gmail.com>
2021-05-13 20:21:47 -0700

    Change Section SVG colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Sections from "./components/Sections/Sections";
 import Info from "./components/Info";
 import Footer from "./components/Footer";
 import Section from './types/Section';
+import './index.scss';
 
 import {ReactComponent as DataSVG} from './assets/svg/data.svg';
 import {ReactComponent as GeographySVG} from './assets/svg/geography.svg';
@@ -13,7 +14,7 @@ import {ReactComponent as ProgramsSVG} from './assets/svg/programs.svg';
 import {ReactComponent as FiscalSVG} from './assets/svg/fiscal.svg';
 
 function App() {
-  const sections = [new Section(<DataSVG />, 'Data & Research'), new Section(<GeographySVG />, 'Geography & Mapping'), new Section(<ProgramsSVG />, 'Programs & Services'), new Section(<FiscalSVG />, 'Legislative Fiscal Impacts')];
+  const sections = [new Section(<DataSVG />, 'Data & Research'), new Section(<GeographySVG fill='#35b0cb'/>, 'Geography & Mapping'), new Section(<ProgramsSVG fill="#d2b859"/>, 'Programs & Services'), new Section(<FiscalSVG fill="#35b0cb"/>, 'Legislative Fiscal Impacts')];
   return (
     <div className={styles.App}>
       <Header />
