fc731c29d8a0956733438e7060b79396c369a49b
Owen Medina <owensmedi@gmail.com>
2021-05-13 17:59:28 -0700

    Set SearchButton type to submit

--- src/components/Buttons/SearchButton.tsx
+++ src/components/Buttons/SearchButton.tsx
@@ -4,7 +4,7 @@ import styles from './SearchButton.module.scss';
 const SearchButton = () => {
     return <button className={styles['button-search']}>
         <span className={`${styles['button-search__text']} ${styles['mr-sm']}`}>Search</span>
-        <SearchSVG className={styles['button-search__svg']}/>
+        <SearchSVG type={'submit'} className={styles['button-search__svg']}/>
         </button>;
 }
 
