985597997afdde2c09573e4056c5aaa894e0c75d
Owen Medina <owensmedi@gmail.com>
2021-05-14 13:13:05 -0700

    Make Link accept other classes as prop

--- src/components/Typography/Link.tsx
+++ src/components/Typography/Link.tsx
@@ -4,10 +4,13 @@ import styles from './Link.module.scss';
 interface Props {
     text: string;
     reference: string;
+    otherClasses: string[];
 }
 
-const Link: FC<Props> = ({text, reference}) => {
-    return <a className={styles.link} href={reference}>{text}</a>;
+const Link: FC<Props> = ({text, reference, otherClasses}) => {
+    const classes = otherClasses.slice(0);
+    classes.push(styles.link);
+    return <a className={classes.join(' ')} href={reference}>{text}</a>;
 }
 
 export default Link;
