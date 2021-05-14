837b240cd204b36b00ea76d9716fd4ef29da8b84
Owen Medina <owensmedi@gmail.com>
2021-05-13 18:13:00 -0700

    Set sections background and refactor name

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './App.module.scss';
 import Header from "./components/Header/Header";
 import MainHeading from "./components/Main Heading/MainHeading";
-import Pages from "./components/Pages";
+import Pages from "./components/Sections/Sections";
 import Info from "./components/Info";
 import Footer from "./components/Footer";
 
---
+++ src/components/Sections/Sections.tsx
@@ -0,0 +1,7 @@
+import styles from './Sections.module.scss';
+
+const Pages = () => {
+    return <div className={styles.pages}></div>;
+}
+
+export default Pages;
