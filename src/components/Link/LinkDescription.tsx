899172b44b4abb72040cb244bcbfbd583fe85067
Owen Medina <owensmedi@gmail.com>
2021-05-14 16:28:11 -0700

    Update component to use updated LinkDescription

--- src/components/Link/LinkDescription.tsx
+++ src/components/Link/LinkDescription.tsx
@@ -1,26 +1,33 @@
 import { FC } from "react";
+import Tag from '../Tag/Tag';
 import styles from './LinkDescription.module.scss';
+import {DescriptionDetails} from '../../types/Link';
 
 interface Props {
-    desc?: string;
-    date: Date;
+    details?: DescriptionDetails;
 }
 
-const LinkDescription: FC<Props> = ({desc, date}) => {
+const LinkDescription: FC<Props> = ({details}) => {
+    if (!details) return <div></div>;
+    
+    const {description, date, tags} = details;
     const year = 'numeric';
     const month = 'short';
     const day = 'numeric';
     const locale = 'en-US'
-    const formattedYear = date.toLocaleDateString(locale, {year});
-    const formattedMonth = date.toLocaleDateString(locale, {month});
-    const formattedDay = date.toLocaleDateString(locale, {day});
+    const formattedYear = date?.toLocaleDateString(locale, {year});
+    const formattedMonth = date?.toLocaleDateString(locale, {month});
+    const formattedDay = date?.toLocaleDateString(locale, {day});
     return <div>
-        {desc && <div className={styles['description__text']}>
-            {desc}
+        {description && <div className={styles['description__text']}>
+            {description}
             </div>}
         <div>
-            <span className={[styles['date__label'], styles.date].join(' ')}>Published: </span>
-            <span className={[styles['date__date'], styles.date].join(' ')}>{`${formattedMonth} ${formattedDay}, ${formattedYear}`}</span>
+            {date && <span>
+                <span className={[styles['date__label'], styles.date].join(' ')}>Published: </span>
+                <span className={[styles['date__date'], styles.date].join(' ')}>{`${formattedMonth} ${formattedDay}, ${formattedYear}`}</span>
+            </span>}
+            {tags && tags.map(tag => <Tag text={tag}/>)}
         </div>
     </div>;
 }
