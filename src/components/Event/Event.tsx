a82ed1433501411ee2be7d6a0ca17f35e72c69cf
Owen Medina <owensmedi@gmail.com>
2021-05-14 20:23:11 -0700

    Allow Event to accept optional classes

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -6,10 +6,13 @@ import styles from './Event.module.scss';
 
 interface Props {
     event: EventClass;
+    otherClasses?: string[];
 }
 
-const Event: FC<Props> = ({event}) => {
-    return <div className={styles.event}>
+const Event: FC<Props> = ({event, otherClasses}) => {
+    const classes = otherClasses ?? [];
+    classes.push(styles.event);
+    return <div className={classes.join(' ')}>
         <EventDate date={event.date} />
         <EventDetails title={event.title} location={event.location} date={event.date} contact={event.contact} tags={event.tags}/>
     </div>;
