d3c21a64e221deb2603b5c2113ddb9da9d9aee3c
Owen Medina <owensmedi@gmail.com>
2021-05-17 11:44:44 -0700

    Add Full Navigation Menu

--- src/components/Header/NavMenuButton.tsx
+++ src/components/Header/NavMenuButton.tsx
@@ -1,17 +1,20 @@
 import styles from './NavMenuButton.module.scss';
 import {ReactComponent as NavMenuIcon} from './assets/svg/menu.svg';
 import { useState } from 'react';
+import NavigationMenu from '../NavMenu/NavigationMenu';
 
 const NavMenuButton = () => {
     const [navMenuOpen, setNavMenuOpen] = useState(false);
 
     const toggleNavMenu = () => {
-        setNavMenuOpen(prev => !prev);
+        console.log('nav menu button pressed')
+        setNavMenuOpen(prev => true);
     }
 
-    return <button className={styles['nav-menu-button']}>
+    return <button className={styles['nav-menu-button']} onClick={toggleNavMenu}>
         <NavMenuIcon className={styles['nav-menu-icon']}/>
         <div className={styles['nav-menu-title']}>MENU</div>
+        <NavigationMenu />
     </button>;
 }
 
