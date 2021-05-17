2dd695e0d917cefc48a29762359d8a7fcc7f4664
Owen Medina <owensmedi@gmail.com>
2021-05-17 10:06:02 -0700

    Change NavMenuButton to button (semantics)

--- src/components/Header/NavMenuButton.tsx
+++ src/components/Header/NavMenuButton.tsx
@@ -1,11 +1,18 @@
 import styles from './NavMenuButton.module.scss';
 import {ReactComponent as NavMenuIcon} from './assets/svg/menu.svg';
+import { useState } from 'react';
 
 const NavMenuButton = () => {
-    return <div className={styles['nav-menu-button']}>
+    const [navMenuOpen, setNavMenuOpen] = useState(false);
+
+    const toggleNavMenu = () => {
+        setNavMenuOpen(prev => !prev);
+    }
+
+    return <button className={styles['nav-menu-button']}>
         <NavMenuIcon className={styles['nav-menu-icon']}/>
         <div className={styles['nav-menu-title']}>MENU</div>
-    </div>;
+    </button>;
 }
 
 export default NavMenuButton;
