0cbdb3c68d882838802d72542e389fed2e275768
Owen Medina <owensmedi@gmail.com>
2021-05-18 12:04:11 -0700

    Fix nested button element

--- src/components/Buttons/SearchIconButton.tsx
+++ src/components/Buttons/SearchIconButton.tsx
@@ -6,17 +6,16 @@ import SearchForm from '../Forms/SearchForm';
 const SearchIconButton = () => {
     const [searchOpen, setSearchOpen] = useState(false);
 
-    const toggleSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if ((e.target as HTMLButtonElement).tagName !== 'INPUT')setSearchOpen(prev => !prev);
-        console.log('search pressed', (e.target as HTMLButtonElement).tagName);
+    const toggleSearch = (e: React.MouseEvent<HTMLDivElement>) => {
+        if ((e.target as HTMLDivElement).tagName !== 'INPUT')setSearchOpen(prev => !prev);
     }
 
-    return <button className={[styles['button-icon-search'], styles[`button-icon-search--${searchOpen ? 'open' : 'close'}`], styles['mr-sm']].join(" ")} onClick={toggleSearch}>
+    return <div role={'button'} className={[styles['button-icon-search'], styles[`button-icon-search--${searchOpen ? 'open' : 'close'}`], styles['mr-sm']].join(" ")} onClick={toggleSearch}>
         <SearchSVG className={styles['search-svg']}/>
         {searchOpen && <div className={styles['search-form--nav']}>
             <SearchForm placeholder={'Enter search term'} buttonText={'Search'} buttonStyle={2} />
         </div>}
-    </button>;
+    </div>;
 }
 
 export default SearchIconButton;
