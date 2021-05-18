95aa75309dba885d8abc948ae06a2e18913c5c80
Owen Medina <owensmedi@gmail.com>
2021-05-17 19:06:44 -0700

    Allow SolidButton to accept optional classes

--- src/components/Buttons/SolidButton.tsx
+++ src/components/Buttons/SolidButton.tsx
@@ -3,10 +3,13 @@ import styles from './SolidButton.module.scss';
 
 interface Props {
     text?: string;
+    otherClasses?: string[];
 }
 
-const SolidButton: FC<Props> = ({text}) => {
-    return <button className={styles['button-solid']}>{text}</button>;
+const SolidButton: FC<Props> = ({text, otherClasses}) => {
+    const classes = otherClasses ?? [];
+    classes.push(styles['button-solid']);
+    return <button className={classes.join(" ")}>{text}</button>;
 }
 
 export default SolidButton;
