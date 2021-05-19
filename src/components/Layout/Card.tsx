738f4ae88ff390af4f115388e9f9e8479c78b927
Owen Medina <owensmedi@gmail.com>
2021-05-18 18:15:07 -0700

    Adjust sizing for xxs breakpoint

--- src/components/Layout/Card.tsx
+++ src/components/Layout/Card.tsx
@@ -7,10 +7,13 @@ import '../../index.scss';
 interface Props {
     icon: ReactElement;
     title: string;
+    otherClasses?: string[];
 }
 
-const Card: FC<Props> = ({icon, title}) => {
-    return <div className={styles.card}>
+const Card: FC<Props> = ({icon, title, otherClasses}) => {
+    const classes = otherClasses ?? [];
+    classes.push(styles.card);
+    return <div className={classes.join(' ')}>
         {icon}
         <h3 className={styles['card__title']}>{title}</h3>
     </div>;
--- src/components/Sections/Sections.tsx
+++ src/components/Sections/Sections.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Sections: React.FC<Props> = ({sections}) => {
     return <div className={styles.sections}>
-        {sections.map(section => <Card icon={section.icon} title={section.title} key={section.id}/>)}
+        {sections.map(section => <Card icon={section.icon} title={section.title} key={section.id} otherClasses={[styles.section]}/>)}
     </div>;
 }
 
