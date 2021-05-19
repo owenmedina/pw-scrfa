c206df81ea37d4667142025862a6a8d4eb94949b
Owen Medina <owensmedi@gmail.com>
2021-05-18 20:26:18 -0700

    Remove aria attributes from main-heading Search

--- src/components/Buttons/SearchButton.tsx
+++ src/components/Buttons/SearchButton.tsx
@@ -2,7 +2,7 @@ import {ReactComponent as SearchSVG} from './assets/svg/search.svg';
 import styles from './SearchButton.module.scss';
 
 const SearchButton = () => {
-    return <button className={styles['button-search']} aria-label={'main-heading-search'} role={'search'}>
+    return <button className={styles['button-search']}>
         <span className={`${styles['button-search__text']} ${styles['mr-sm']}`}>Search</span>
         <SearchSVG type={'submit'} className={styles['button-search__svg']}/>
         </button>;
