b4bc2fddc2afefd9f6c75186ec7fc54e94105ea9
Owen Medina <owensmedi@gmail.com>
2021-05-18 20:16:42 -0700

    Add roles to Header and NavLinks

--- src/components/Header/NavLinks.tsx
+++ src/components/Header/NavLinks.tsx
@@ -1,11 +1,11 @@
 import styles from './NavLinks.module.scss';
 
 const NavLinks = () => {
-    return <div className={styles['nav-links']}>
+    return <nav className={styles['nav-links']}>
         <span className={styles['nav-link']}>About Us</span>
         <span className={styles['nav-link']}>Events</span>
         <span className={styles['nav-link']}>Boards &amp; Committees</span>
-    </div>;
+    </nav>;
 }
 
 export default NavLinks;
